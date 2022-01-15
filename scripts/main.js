bafe582d2f40da3cc5d1a3d9f6d959ffa6082d58
YueLiPicasso <yue.li.picasso@qq.com>
2022-01-16 02:38:07 +0300

    font choice

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
+function fontWithFallback(myfont) {
+    switch (myfont) {
+    case 'Choose English Font':
+    case '选择中文字体':
+	return 'serif';
+    default:
+        return myfont + ', serif';
+    }
+}
 
+function changeFont(element, name) {
+    element.style.fontFamily = name;
+}
+
+function favef() {
+    let mylist = document.getElementById('EF');
+    let mychoice = mylist.options[mylist.selectedIndex].text;
+    let ft = fontWithFallback(mychoice);
+    let myelem = document.querySelector('html');
+    changeFont(myelem, ft);
+}
+
+function favzf() {
+    let mylist = document.getElementById('ZF');
+    let mychoice = mylist.options[mylist.selectedIndex].text;
+    let ft = fontWithFallback(mychoice);
+    let myelems = document.querySelectorAll('[lang="zh-Hans"]');
+    myelems.forEach(function (el) {changeFont(el, ft);});
+}
