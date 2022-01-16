28c48a77831fba90c2c96fea92c941c6383f1fb3
YueLiPicasso <yue.li.picasso@qq.com>
2022-01-16 18:15:32 +0300

    bg img

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,44 @@ function fontWithFallback(myfont) {
     }
 }
 
-function changeFont(element, name) {
-    element.style.fontFamily = name;
+function changeFont(elem, name) {
+    elem.style.fontFamily = name;
 }
+
+function changeBg (name) {
+    let ur;
+    if (name === 'none') {
+	ur = 'none' ;
+    } else {
+	ur  = 'url("./photo/bkg/' + name + '.jpg")' ;
+    }
+    switch (name) {
+    case 'monta':
+	document.body.style.backgroundSize = '100%';
+	document.body.style.backgroundAttachment = 'fixed';
+	document.body.style.backgroundRepeat = 'no-repeat';
+	document.body.style.backgroundPosition = '50% 15%';
+	break;
+    case 'spring':
+	document.body.style.backgroundSize = '100%';
+	document.body.style.backgroundAttachment = 'fixed';
+	break;
+    case 'leafa':
+	document.body.style.backgroundSize = 'cover';
+	document.body.style.backgroundAttachment = 'fixed';
+	break;
+    case 'montb':
+	document.body.style.backgroundSize = '360%';
+	document.body.style.backgroundRepeat = 'no-repeat';
+	document.body.style.backgroundPosition = '8% 45%';
+    default:
+	document.body.style.backgroundAttachment = 'scroll';
+	break;
+    } 
+    document.body.style.backgroundImage = ur;
+    return;
+}
+
 
 function favef() {
     let mylist = document.getElementById('EF');
@@ -26,4 +61,10 @@ function favzf() {
     let ft = fontWithFallback(mychoice);
     let myelems = document.querySelectorAll('[lang="zh-Hans"]');
     myelems.forEach(function (el) {changeFont(el, ft);});
+}
+
+function favbg() {
+    let mylist = document.getElementById('BG');
+    let mychoice = mylist.options[mylist.selectedIndex].text;
+    changeBg(mychoice);
 }
